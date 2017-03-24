a7269461a71fe12d2becc25827fcad00bc1a1513
DK\psk <psk@sitecore.net>
2017-03-24 17:12:37 -0400

    fix for Solr and Lucene

---
+++ src/Sitecore.Support.154030/ContentSearch/ComputedFields/HasChildren.cs
@@ -0,0 +1,34 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.ComputedFields;
+using Sitecore.Data.Items;
+using Sitecore.Data.Managers;
+using Sitecore.SecurityModel;
+
+namespace Sitecore.Support.ContentSearch.ComputedFields
+{
+    public class HasChildren : IComputedIndexField
+    {
+        public string FieldName
+        {
+            get;
+            set;
+        }
+
+        public string ReturnType
+        {
+            get;
+            set;
+        }
+
+        public object ComputeFieldValue(IIndexable indexable)
+        {
+            Item item = indexable as SitecoreIndexableItem;
+            if (item == null)
+            {
+                return null;
+            }
+            SecurityCheck securityCheck = SecurityStateSwitcher.CurrentValue == SecurityState.Disabled ? SecurityCheck.Disable : SecurityCheck.Enable;
+            return ItemManager.HasChildren(item, securityCheck);
+        }
+    }
+}
---
+++ src/Sitecore.Support.154030/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.154030")]
+[assembly: AssemblyProduct("Sitecore.Support.154030")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("HasChildren computed index field causes high memory usage during indexing")]
+
